a49feb1ef047ce5bc70bcb4c14d2bab3b8fec093
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-06 19:51:45 +0700

    Update lab2.php

--- lab2.php
+++ lab2.php
@@ -1,3 +1,69 @@
-<?php
-echo "hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lab2-2</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/staffs/staff4.jpg"
+                    style="height: 100%; width: 100%;" class="img-fluid"/>
+                    <div class="card-header bg-white text-black" >
+                        <h4>Beutiful</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Women</p>
+                        <a href="#" class="btn btn">Read more</a>
+
+                    </div>
+                    <div class="card-footer">
+                        By Palm
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/staffs/staff5.jpg"
+                    style="height: 100%; width: 100%;" class="img-fluid"/>
+                    <div class="card-header bg-white text-black" >
+                        <h4>Beutiful</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Women</p>
+                        <a href="#" class="btn btn">Read more</a>
+                    </div>
+                    <div class="card-footer">
+                        By Palm
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <div class="card">
+                    <img class="card-img-top" src="img/staffs/staff6.jpg"
+                    style="height: 100%; width: 100%;" class="img-fluid"/>
+                    <div class="card-header bg-white text-black" >
+                        <h4>Handsome</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Man</p>
+                        <a href="#" class="btn btn">Read more</a>
+                    </div>
+                    <div class="card-footer">
+                        By Palm
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div> 
+</body>
+</html>
