441fc3d4e5ca16477bb4175ed28808fdb888e356
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-06 19:47:11 +0700

    Update lab1.php

--- lab1.php
+++ lab1.php
@@ -1,3 +1,47 @@
-<?php
-echo "hello";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lab2-1</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                    <img src="img/work/work1.jpg" 
+                    style="height: 100%; width: 100%;"
+                    class="img-fluid"  
+                    />
+            </div>
+            <div class="col-sm-3">
+                <div class="row">
+                    <img src="img/work/work2.jpg" class="img-fluid" 
+                    style="height: 100%; width: 100%;"/>
+                </div>
+                <div class="row">
+                    <img src="img/work/work3.jpg" class="img-fluid" 
+                    style="height: 100%; width: 100%;"/>
+                </div>
+                <div class="row">
+                    <img src="img/work/work4.jpg" class="img-fluid" 
+                    style="height: 100%; width: 100%;"/>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <!-- <div class="row"> -->
+                    <img src="img/work/work5.jpg" class="img-fluid" 
+                    style="height: 66.66%; width: 100%;"/>
+                <!-- </div> -->
+                <div class="row">
+                    <img src="img/work/work6.jpg" class="img-fluid" 
+                    style="height: 100%; width: 100%;"/>
+                </div>
+            </div>
+        </div>
+
+    </div> <!-- Close container -->
+</body>
+</html>
